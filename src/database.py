bf34f6bab2f04a9ff038f6741c2bdfbb46a4548a
kitze012 <sskitzerow@gmail.com>
2023-12-05 14:30:15 -0600

    Adding update function to database.py

--- src/database.py
+++ src/database.py
@@ -20,41 +20,81 @@ def add_card(category:str, question:str, code:str, image:str, answer:str):
         image (str): a filepath to an image that supports the question
         answer (str): the expected response to the question
 
-    Output(s): returns if the data is inserted into the database, else returns false
+    Output(s): 
+        returns if the data is inserted into the database, else returns false
     '''
     try:
-        conn = sqlite3.connect('flashcards.db')     # Connection to the database
-        c = conn.cursor()
-        c.execute(f"INSERT INTO Flashcards VALUES (?,?,?,?,?)", (category,question,code,image,answer))
-        conn.commit()                               # Commit changes to database
+        with sqlite3.connect('flashcards.db') as conn:      # Connection to the database
+            LOGGER.info(f"Adding the following row to the database:\n{category}, {question}, {code}, {image}, {answer}")
+
+            c = conn.cursor()
+            c.execute("INSERT INTO Flashcards VALUES (?,?,?,?,?)", (category,question,code,image,answer))
+            conn.commit()                                   # Commit changes to database
 
         return True
     
     except sqlite3.Error as e:
         LOGGER.error(f"An error occured when adding to the database: {e}")
         return False
+
+# ==============================================================================================================
+def view_card(question):
+    '''
+    Adds the flashcard data to the database.
+
+    Parameter(s):
+        question (str): information being asked
+
+    Output(s):
+        returns a tuple of the Flashcard data if found, None otherwise
+    '''
+    try:
+        with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
+            LOGGER.info(f"Searching the database for the following question:\n{question}")
+
+            c = conn.cursor()
+            c.execute("SELECT * FROM Flashcards WHERE question = ?", (question,))
+            data = c.fetchone()
+            conn.commit()
+
+        return data
     
-    finally:
-        if conn:
-            conn.close()                            # Close connection to the database
+    except sqlite3.Error as e:
+        LOGGER.error(f"An error occured when searching the database: {e}")
+        return None
 
 # ==============================================================================================================
-def view_card():
-    conn = sqlite3.connect('flashcards.db')     # Connection to the database
-    c = conn.cursor()
+def update_card(old_question: str, new_data: dict):
+    '''
+    Updates flashcard data in the database.
 
-    conn.commit()                               # Commit changes to database
-    conn.close()                                # Close connection to the database
-    return
+    Parameters:
+        old_question (str): the original question identifying the flashcard to be updated
+        new_data (dict): a dictionary containing the new data for the flashcard
+            {'category': value, 'question': value, 'code', value, 'image': value, 'answer':value}
 
-# ==============================================================================================================
-def update_card():
-    conn = sqlite3.connect('flashcards.db')     # Connection to the database
-    c = conn.cursor()
+    Returns:
+        bool: True if the update was successful, False otherwise
+    '''
+    try:
+        with sqlite3.connect('flashcards.db') as conn:
+            LOGGER.info(f"Updating the following quetion with new data:\nOld Question: {old_question}\nNew Data: {new_data}")
 
-    conn.commit()                               # Commit changes to database
-    conn.close()                                # Close connection to the database
-    return
+            c = conn.cursor()
+            # Construct the SET clause dynamically based on the new_data dictionary
+            set_clause = ', '.join(f"{key} = ?" for key in new_data.keys())
+
+            # Build the query and execute
+            query = f"UPDATE Flashcards SET {set_clause} WHERE question = ?"
+            c.execute(query, (old_question,))
+
+            conn.commit()  # Commit changes to the database
+
+        return True
+    
+    except sqlite3.Error as e:
+        LOGGER.error(f"An error occurred when updating the database: {e}")
+        return False
 
 # ==============================================================================================================
 def delete_card():
