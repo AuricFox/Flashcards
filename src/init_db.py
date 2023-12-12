239d64380fa3324b9d3f06e69bf3a07672f27b0b
kitze012 <sskitzerow@gmail.com>
2023-12-11 22:10:47 -0600

    Adding PK variable

--- src/init_db.py
+++ src/init_db.py
@@ -7,17 +7,12 @@ def create_tables():
 
     # Main flashcard elements
     c.execute("""CREATE TABLE Flashcards(
+              key INTEGER PRIMARY KEY AUTOINCREMENT,
               category TEXT NOT NULL,
               question TEXT NOT NULL,
               code TEXT.
               image_path TEXT,
               answer TEXT NOT NULL
-    )""")
-
-    # Track the number of questions in each category
-    c.execute("""CREATE TABLE Category(
-              category TEXT NOT NULL,
-              count INTEGER
     )""")
 
     conn.commit()                               # Commit changes to database
