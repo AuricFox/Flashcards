aea319f8c90339e09596472b097fcdbca035b8bf
kitze012 <sskitzerow@gmail.com>
2023-12-26 02:50:58 -0600

    Finished refactoring database.py to utilize updated tables

--- src/database.py
+++ src/database.py
@@ -5,7 +5,8 @@ LOGGER = utils.LOGGER
 # ==============================================================================================================
 def add_card(category:str, question:str, answer:str, code:str=None, code_type:str=None, image_file:str=None):
     '''
-    Adds the flashcard data to the database.
+    Adds the flashcard data to the database. User can only store code elements or an image and not both. This prevents 
+    the flashcard from becoming too cluttered (NOTE: may change in the future).
 
     Parameter(s):
         category (str): states what the question is related to
@@ -29,6 +30,7 @@ def add_card(category:str, question:str, answer:str, code:str=None, code_type:st
                 code_set = (code, code_type)
 
                 c_code = conn.cursor()
+                LOGGER.info(code_query, code_set)
                 c_code.execute(code_query, code_set)
                 conn.commit()
 
@@ -49,11 +51,10 @@ def add_card(category:str, question:str, answer:str, code:str=None, code_type:st
                 flashcard_query = "INSERT INTO Flashcards (category, question, answer) VALUES (?,?,?)"
                 flashcard_set = (category, question, answer)
 
-            LOGGER.info(f"{flashcard_query}\n{flashcard_set}")
-
             c = conn.cursor()
+            LOGGER.info(flashcard_query, flashcard_set)
             c.execute(flashcard_query, flashcard_set)
-            conn.commit()                                   # Commit changes to database
+            conn.commit()
 
         return True
     
@@ -62,17 +63,17 @@ def add_card(category:str, question:str, answer:str, code:str=None, code_type:st
         return False
 
 # ==============================================================================================================
-def view_card(fid:int):
+def view_card(key:int):
     '''
     Retrieves the flashcard data with the queried primary key from the database.
 
     Parameter(s):
-        fid (str): the primary key of the flashcard being queried
+        key (str): the primary key of the flashcard being queried
 
     Output(s):
-        data (dict): a dictionary of the Flashcard data if found, None otherwise
+        response (dict): a dictionary of the Flashcard data if found, None otherwise
 
-        data = {
+        response = {
             'fid':int, 
             'category':str, 
             'question':str, 
@@ -84,16 +85,14 @@ def view_card(fid:int):
         }
     '''
     try:
-        with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
-            LOGGER.info(f"SELECT * FROM Flashcards WHERE fid = {fid}")
-
+        with sqlite3.connect('flashcards.db') as conn:
             c = conn.cursor()
-            c.execute("SELECT * FROM Flashcards WHERE fid = ?", (fid,))
-            # Get the flash data: (category, question, code, image_file, answer)
+            LOGGER.info("SELECT * FROM Flashcards WHERE fid = ?", (key,))
+            c.execute("SELECT * FROM Flashcards WHERE fid = ?", (key,))
             card_data = c.fetchone()
 
             # Convert the tuple into a dictionary
-            data = {
+            response = {
                 'fid': card_data[0],
                 'category': card_data[1],
                 'question': card_data[2], 
@@ -103,15 +102,15 @@ def view_card(fid:int):
             }
 
             # Get code elements if code_id is not null
-            if data['code_id'] is not None:
-                LOGGER.info(f"SELECT * FROM Code WHERE cid = {data['code_id']}")
-                c.execute("SELECT * FROM Code WHERE cid = ?", (data['code_id'],))
+            if response['code_id'] is not None:
+                LOGGER.info("SELECT * FROM Code WHERE cid = ?", (response['code_id'],))
+                c.execute("SELECT * FROM Code WHERE cid = ?", (response['code_id'],))
                 code_data = c.fetchone()
 
-                data['code'] = code_data[1]
-                data['code_type'] = code_data[2]
+                response['code'] = code_data[1]
+                response['code_type'] = code_data[2]
 
-        return data
+        return response
     
     except sqlite3.Error as e:
         LOGGER.error(f"An error occured when searching the database: {e}")
@@ -125,28 +124,28 @@ def view_allcategories():
     Parameter(s): None
 
     Output(s):
-        data (dict): a dictionary containing the question category and its count if successful, an empty dictionary otherwise
+        response (dict): a dictionary containing the question category and its count if successful, none otherwise
 
-        data = {'category': count})
+        response = {'category': count}
     '''
     try:
-        with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
-            LOGGER.info(f"SELECT category, COUNT(*) as count FROM Flashcards GROUP BY category")
-
+        with sqlite3.connect('flashcards.db') as conn:
             c = conn.cursor()
+
+            LOGGER.info(f"SELECT category, COUNT(*) as count FROM Flashcards GROUP BY category")
             c.execute("SELECT category, COUNT(*) as count FROM Flashcards GROUP BY category")
             card_data = c.fetchall()
 
-            data = {}
+            response = {}
             for x in card_data:
-                key = x[0]          # Category type
-                data[key] = x[1]    # Number of question in the category
+                key = x[0]              # Category type
+                response[key] = x[1]    # Number of question in the category
 
-        return data
+        return response
     
     except sqlite3.Error as e:
         LOGGER.error(f"An error occured when retrieving from the database: {e}")
-        return {}
+        return None
        
 # ==============================================================================================================
 def view_allcards(category:str=None):
@@ -157,11 +156,10 @@ def view_allcards(category:str=None):
         category (str, defualt=None): specifies which group of cards to retrieve
 
     Output(s):
-        data (dict): a dictionary containing flashcard data if successful, an dictionary with an empty list otherwise
+        response (dict): a dictionary containing flashcard data if successful, none otherwise
 
-        data = {
-            'questions': [
-                {
+        response = {
+            'questions': [{
                 'key':int, 
                 'category':str, 
                 'question':str, 
@@ -170,8 +168,7 @@ def view_allcards(category:str=None):
                 'image_file':str, 
                 'code':str if code_id not null,
                 'code_type':str if code_id not null
-                }, ...
-            ]}
+            }, ... ]}
     '''
     try:
         with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
@@ -179,7 +176,7 @@ def view_allcards(category:str=None):
 
             # Retrieve all cards in a specified category
             if category:
-                LOGGER.info(f"SELECT * FROM Flashcards WHERE category = {category}")
+                LOGGER.info("SELECT * FROM Flashcards WHERE category = ?", (category,))
                 c.execute("SELECT * FROM Flashcards WHERE category = ?", (category,))
             # Retrieve all cards in the database
             else:
@@ -189,32 +186,38 @@ def view_allcards(category:str=None):
             card_data = c.fetchall()
 
             # Convert the list of tuples into a dictionary
-            data = {'questions': []}
-            for q in card_data:
+            response = {'questions': []}
+            for data in card_data:
                 question = {
-                    'fid': q[0], 
-                    'category': q[1], 
-                    'question': q[2], 
-                    'answer': q[3], 
-                    'code_id': q[4], 
-                    'image_file': q[5]
+                    'fid': data[0], 
+                    'category': data[1], 
+                    'question': data[2], 
+                    'answer': data[3], 
+                    'code_id': data[4], 
+                    'image_file': data[5]
                 }
                 
-                
-                
-                data['questions'].append(question)
+                # Retrieve code elements if there is a code_id
+                if question['code_id'] is not None:
+                    c.execute("SELECT * FROM Code WHERE cid = ?", (question['code_id'],))
+                    code_data = c.fetchone()
 
+                    question['code'] = code_data[1]
+                    question['code_type'] = code_data[2]
+                
+                response['questions'].append(question)
 
-        return data
+        return response
     
     except sqlite3.Error as e:
         LOGGER.error(f"An error occured when retrieving from the database: {e}")
-        return []
+        return None
 
 # ==============================================================================================================
 def update_card(key:int, category:str, question:str, answer:str, code:str=None, code_type:str=None, image_file:str=None):
     '''
-    Updates flashcard data in the database.
+    Updates flashcard data in the database. User can only store code elements or an image and not both. This prevents 
+    the flashcard from becoming too cluttered (NOTE: may change in the future).
 
     Parameters:
         key (int): the primary key of the flashcard being updated
@@ -233,36 +236,56 @@ def update_card(key:int, category:str, question:str, answer:str, code:str=None, 
             # Sanitize category of special characters
             category = utils.sanitize(category)
 
-            # Construct the SET clause based on the new_data dictionary
-            set_clause = 'category = ?, question = ?, code = ?, answer = ?'
-            set_query = (category, question, answer)
-
             c = conn.cursor()
             # Get the current image filename and code block if they exist
-            c.execute("SELECT image_file, code FROM Flashcards WHERE key = ?", (key,))
+            LOGGER.info("SELECT image_file, code_id FROM Flashcards WHERE key = ?", (key,))
+            c.execute("SELECT image_file, code_id FROM Flashcards WHERE key = ?", (key,))
             current_data= c.fetchone()
             current_image_file = current_data[0]
-            current_code = current_data[1]
+            current_code_id = current_data[1]
             
-            # Update image file if one has been submitted
+            # Update image file if one has been submitted along with other elements
             if image_file:
+                # Delete current image from image folder
+                if current_image_file: utils.remove_image(current_image_file)
+                # Delete current code from the database
+                if current_code_id: delete_code(current_code_id)
 
+                flashcard_set = (category, question, answer, None, image_file, key)
+
+            # Update code block and code type if changed along with other elements
+            elif code and code_type:
                 # Delete current image from image folder
                 if current_image_file: utils.remove_image(current_image_file)
 
-                set_clause += ', image_file = ?'
-                set_query += (image_file,)
+                # Update code elements if a code_id exists
+                if current_code_id:
+                    code_query = "UPDATE Code SET code_block = ?, code_type = ? WHERE cid = ?"
+                    code_set = (code, code_type, current_code_id)
+                # Add new code elements to the code table if code_id does not exist
+                else:
+                    code_query = "INSERT INTO Code (code_block, code_type) VALUES (?,?)"
+                    code_set = (code, code_type)
 
-            set_query += (key,)
+                # Commit code element changes
+                c_code = conn.cursor()
+                LOGGER.info(code_query, code_set)
+                c_code.execute(code_query, code_set)
+                conn.commit()
 
-            LOGGER.info(f"UPDATE Flashcards SET {set_clause} WHERE key = ?\n{set_query}")
+                # Retrieve the generated primary key (ckey) from the Code table
+                current_code_id = c_code.lastrowid
+                flashcard_set = (category, question, answer, current_code_id, None, key)
 
-            
-            # Build the query and execute
-            query = f"UPDATE Flashcards SET {set_clause} WHERE key = ?"
-            c.execute(query, set_query)
+            # Update basic elements
+            else:
+                flashcard_set = (category, question, answer, None, None, key)
 
-            conn.commit()  # Commit changes to the database
+            flashcard_query = "UPDATE Flashcards SET category = ?, question = ?, answer = ?, code_id = ?, image_file = ? WHERE fid = ?"
+
+            LOGGER.info(flashcard_query, flashcard_set)
+            c.execute(flashcard_query, flashcard_set)
+            conn.commit()
 
         return True
     
@@ -276,21 +299,46 @@ def delete_card(key:int):
     Deletes the flashcard data from the database.
 
     Parameter(s):
-        question (str): variable being queried from the database for deletion
+        key (int): the primary key of the flashcard being deleted from the database
 
     Output(s):
-        Bool: True if the delete was successful, False otherwise
+        Bool: True if the deletion was successful, False otherwise
     '''
     try:
         with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
-            LOGGER.info(f"DELETE FROM Flashcards WHERE key = {key}")
-
             c = conn.cursor()
-            c.execute("DELETE FROM Flashcards WHERE key = ?", (key,))
+
+            LOGGER.info(f"DELETE FROM Flashcards WHERE fid = ?", (key,))
+            c.execute("DELETE FROM Flashcards WHERE fid = ?", (key,))
             conn.commit()
 
         return True
     
     except sqlite3.Error as e:
         LOGGER.error(f"An error occured when deleting the flashcard from the database: {e}")
+        return False
+    
+# ==============================================================================================================
+def delete_code(key:int):
+    '''
+    Deletes the code data from the database.
+
+    Parameter(s):
+        key (int): the primary key of the code being deleted from the database
+
+    Output(s):
+        Bool: True if the deletion was successful, False otherwise
+    '''
+    try:
+        with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
+            c = conn.cursor()
+
+            LOGGER.info(f"DELETE FROM Code WHERE cid = ?", (key,))
+            c.execute("DELETE FROM Code WHERE cid = ?", (key,))
+            conn.commit()
+
+        return True
+    
+    except sqlite3.Error as e:
+        LOGGER.error(f"An error occured when deleting the code from the database: {e}")
         return False
