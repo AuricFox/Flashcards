885c0b9b8406eed80c91500e4407cc1e75645c48
kitze012 <sskitzerow@gmail.com>
2023-12-25 23:06:47 -0600

    Refactoring database queries to use updated tables

--- src/database.py
+++ src/database.py
@@ -22,23 +22,37 @@ def add_card(category:str, question:str, answer:str, code:str=None, code_type:st
         with sqlite3.connect('flashcards.db') as conn:      # Connection to the database
             category = utils.sanitize(category)             # Sanitizing category before adding
 
-            # Build parameterized query
-            query = "INSERT INTO Flashcards (category, question, answer) VALUES (?,?,?)"
-            query_set = (category, question, answer)
-
-            # Add code and type to the query
+            # Build query using code example and code type
             if code and code_type:
-                query = "INSERT INTO Flashcards (category, question, answer, code, code_type) VALUES (?,?,?,?,?)"
-                query_set += (code, code_type)
-            # Add the image filename to the query
-            elif image_file:
-                query = "INSERT INTO Flashcards (category, question, answer, image_file) VALUES (?,?,?,?)"
-                query_set += (image_file,)
+                # Add code elements to the database
+                code_query = "INSERT INTO Code (code_block, code_type) VALUES (?,?)"
+                code_set = (code, code_type)
 
-            LOGGER.info(f"{query}\n{query_set}")
+                c_code = conn.cursor()
+                c_code.execute(code_query, code_set)
+                conn.commit()
+
+                # Retrieve the generated primary key (ckey) from the Code table
+                cid = c_code.lastrowid
+
+                # Insert flashcard information with the ckey as the foreign key
+                flashcard_query = "INSERT INTO Flashcards (category, question, answer, code_id) VALUES (?,?,?,?)"
+                flashcard_set = (category, question, answer, cid)
+
+            # Build query using image file
+            elif image_file:
+                flashcard_query = "INSERT INTO Flashcards (category, question, answer, image_file) VALUES (?,?,?,?)"
+                flashcard_set = (category, question, answer, image_file)
+
+            # Build query without code and image examples
+            else:
+                flashcard_query = "INSERT INTO Flashcards (category, question, answer) VALUES (?,?,?)"
+                flashcard_set = (category, question, answer)
+
+            LOGGER.info(f"{flashcard_query}\n{flashcard_set}")
 
             c = conn.cursor()
-            c.execute(query, query_set)
+            c.execute(flashcard_query, flashcard_set)
             conn.commit()                                   # Commit changes to database
 
         return True
@@ -48,35 +62,54 @@ def add_card(category:str, question:str, answer:str, code:str=None, code_type:st
         return False
 
 # ==============================================================================================================
-def view_card(key:int):
+def view_card(fid:int):
     '''
     Retrieves the flashcard data with the queried primary key from the database.
 
     Parameter(s):
-        key (str): the primary key of the flashcard being queried
+        fid (str): the primary key of the flashcard being queried
 
     Output(s):
-        data (dict={'key':int, 'category':str, 'question':str, 'code':str, 'image_file':str, 'answer':str}): returns a 
-        dictionary of the Flashcard data if found, None otherwise
+        data (dict): a dictionary of the Flashcard data if found, None otherwise
+
+        data = {
+            'fid':int, 
+            'category':str, 
+            'question':str, 
+            'answer':str, 
+            'code_id':str, 
+            'image_file':str, 
+            'code':str if code_id not null,
+            'code_type':str if code_id not null
+        }
     '''
     try:
         with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
-            LOGGER.info(f"SELECT * FROM Flashcards WHERE key = {key}")
+            LOGGER.info(f"SELECT * FROM Flashcards WHERE fid = {fid}")
 
             c = conn.cursor()
-            c.execute("SELECT * FROM Flashcards WHERE key = ?", (key,))
+            c.execute("SELECT * FROM Flashcards WHERE fid = ?", (fid,))
             # Get the flash data: (category, question, code, image_file, answer)
             card_data = c.fetchone()
 
             # Convert the tuple into a dictionary
             data = {
-                'key': card_data[0],
+                'fid': card_data[0],
                 'category': card_data[1],
                 'question': card_data[2], 
-                'code': card_data[3],
-                'image_file': card_data[4],
-                'answer': card_data[5]
+                'answer': card_data[3],
+                'code_id': card_data[4],
+                'image_file': card_data[5]
             }
+
+            # Get code elements if code_id is not null
+            if data['code_id'] is not None:
+                LOGGER.info(f"SELECT * FROM Code WHERE cid = {data['code_id']}")
+                c.execute("SELECT * FROM Code WHERE cid = ?", (data['code_id'],))
+                code_data = c.fetchone()
+
+                data['code'] = code_data[1]
+                data['code_type'] = code_data[2]
 
         return data
     
@@ -92,8 +125,9 @@ def view_allcategories():
     Parameter(s): None
 
     Output(s):
-        data (dict = {'category': count}): a dictionary containing the question category and its count if successful, 
-        an empty dictionary otherwise
+        data (dict): a dictionary containing the question category and its count if successful, an empty dictionary otherwise
+
+        data = {'category': count})
     '''
     try:
         with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
@@ -123,8 +157,21 @@ def view_allcards(category:str=None):
         category (str, defualt=None): specifies which group of cards to retrieve
 
     Output(s):
-        data (dict={'questions':[{'key':int, 'category':str, 'question':str, 'code':str, 'image_file':str, 'answer':str},...]}): a dictionary 
-        containing flashcard data if successful, an dictionary with an empty list otherwise
+        data (dict): a dictionary containing flashcard data if successful, an dictionary with an empty list otherwise
+
+        data = {
+            'questions': [
+                {
+                'key':int, 
+                'category':str, 
+                'question':str, 
+                'answer':str, 
+                'code_id':str, 
+                'image_file':str, 
+                'code':str if code_id not null,
+                'code_type':str if code_id not null
+                }, ...
+            ]}
     '''
     try:
         with sqlite3.connect('flashcards.db') as conn:    # Connection to the database
@@ -144,7 +191,17 @@ def view_allcards(category:str=None):
             # Convert the list of tuples into a dictionary
             data = {'questions': []}
             for q in card_data:
-                question = {'key': q[0], 'category': q[1], 'question': q[2], 'code': q[3], 'image_file': q[4], 'answer': q[5]}
+                question = {
+                    'fid': q[0], 
+                    'category': q[1], 
+                    'question': q[2], 
+                    'answer': q[3], 
+                    'code_id': q[4], 
+                    'image_file': q[5]
+                }
+                
+                
+                
                 data['questions'].append(question)
 
 
