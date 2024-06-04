4b4de2d25993eedc173bc456e08b8bffed7c1d05
kitze012 <sskitzerow@gmail.com>
2024-06-04 01:13:35 -0500

    Updating flashcard form with radio field and new validation

--- app/forms/flashcard_form.py
+++ app/forms/flashcard_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FileField, HiddenField
+from wtforms import StringField, TextAreaField, FileField, RadioField
 from wtforms.validators import DataRequired, Optional, Length
 
 class FlashcardForm(FlaskForm):
@@ -15,29 +15,37 @@ class FlashcardForm(FlaskForm):
     answer = TextAreaField(
         "Answer", validators=[Optional()]
     )
-    question_code_type = StringField(
+
+    q_figure_type = RadioField('Question Figure Type', choices=[
+        ('code', 'Code'),
+        ('image', 'Image'),
+        ('none', 'None')
+    ], default='none', validators=[DataRequired()])
+
+    q_code_type = StringField(
         "Question Code Type", validators=[Optional(), Length(max=25)]
     )
-    question_code_example = TextAreaField(
+    q_code_example = TextAreaField(
         "Question Code Example", validators=[Optional()]
     )
-    question_image_file = FileField(
+    q_image_file = FileField(
         "Question Image", validators=[Optional()]
     )
-    old_question_image = HiddenField(
-        "Old Question Image"
-    )
-    answer_code_type = StringField(
+
+    a_figure_type = RadioField('Answer Figure Type', choices=[
+        ('code', 'Code'),
+        ('image', 'Image'),
+        ('none', 'None')
+    ], default='none', validators=[DataRequired()])
+
+    a_code_type = StringField(
         "Answer Code Type", validators=[Optional(), Length(max=25)]
     )
-    answer_code_example = TextAreaField(
+    a_code_example = TextAreaField(
         "Answer Code Example", validators=[Optional()]
     )
-    answer_image_file = FileField(
+    a_image_file = FileField(
         "Answer Image", validators=[Optional()]
-    )
-    old_answer_image = HiddenField(
-        "Old Answer Image"
     )
 
     # ==============================================================================================================
@@ -48,43 +56,88 @@ class FlashcardForm(FlaskForm):
         initial_validation = super(FlashcardForm, self).validate(extra_validators)
         if not initial_validation:
             return False
-        
+        #----------------------------------------------------------------------------
+        # Validate Question attributes
+        #----------------------------------------------------------------------------
         # Check if there is anything on the question side of the flashcard
-        if not (self.question and self.question_code_example and self.question_image_file):
-            self.question.errors.append("Please enter a question.")
+        if not (self.question and self.q_code_example and self.q_image_file):
+            self.question.errors.append("Missing question inputs!")
+            return False
+        
+        # Check if the proper figure type has been selected
+        if self.q_figure_type.data == 'code' and self.q_image_file.data:
+            self.q_image_file.errors.append("Cannot submit an image if code is selected!")
+            return False
+        elif self.q_figure_type.data == 'image' and self.q_code_example.data:
+            self.q_code_example.errors.append("Cannot submit code if image is selected!")
+            return False
+        elif self.q_figure_type.data == 'none' and self.q_image_file.data:
+            self.q_image_file.errors.append("Cannot submit an image if none is selected!")
+            return False
+        elif self.q_figure_type.data == 'none' and self.q_code_example.data:
+            self.q_code_example.errors.append("Cannot submit code if none is selected!")
             return False
         
         # Check if there is a code type and example for questions
-        if not self.question_code_type and self.question_code_example:
-            self.question_code_type.errors.append("Code type is required!")
+        if self.q_figure_type.data == 'code' and not self.q_code_type and not self.q_code_example:
+            self.q_code_type.errors.append("Code type is required!")
+            self.q_code_example.errors.append("Code example is required!")
             return False
-        elif self.question_code_type and not self.question_code_example:
-            self.question_code_example.errors.append("Code example is required!")
+        elif self.q_figure_type.data == 'code' and not self.q_code_type and self.q_code_example:
+            self.q_code_type.errors.append("Code type is required!")
+            return False
+        elif self.q_figure_type.data == 'code' and self.q_code_type and not self.q_code_example:
+            self.q_code_example.errors.append("Code example is required!")
+            return False
+        
+        # Check if there is an image if the image field is selected
+        if self.q_figure_type.data == 'image' and not self.q_image_file.data:
+            self.q_image_file.errors.append("Image is required if selected!")
             return False
         
         # Check if there is only one example for questions
-        if self.question_code_example and self.question_image_file:
-            self.question_image_file.errors.append("Can't have image and code examples!")
-            self.question_code_example.errors.append("Can't have code and image examples!")
+        if self.q_code_example and self.q_image_file:
+            self.q_image_file.errors.append("Can't have image and code examples!")
+            self.q_code_example.errors.append("Can't have code and image examples!")
+            return False
+        #----------------------------------------------------------------------------
+        # Validate Answer attributes
+        #----------------------------------------------------------------------------
+        # Check if there is anything on the answer side of the flashcard
+        if not (self.answer and self.a_code_example and self.a_image_file):
+            self.answer.errors.append("Missing answer inputs!")
             return False
         
-        # Check if there is anything on the answer side of the flashcard
-        if not (self.answer and self.answer_code_example and self.answer_image_file):
-            self.answer.errors.append("Please enter an answer.")
+        # Check if the proper figure type has been selected
+        if self.a_figure_type.data == 'code' and self.a_image_file.data:
+            self.a_image_file.errors.append("Cannot submit an image if code is selected!")
+            return False
+        elif self.a_figure_type.data == 'image' and self.a_code_example.data:
+            self.a_code_example.errors.append("Cannot submit code if image is selected!")
+            return False
+        elif self.a_figure_type.data == 'none' and self.a_image_file.data:
+            self.a_image_file.errors.append("Cannot submit an image if none is selected!")
+            return False
+        elif self.a_figure_type.data == 'none' and self.a_code_example.data:
+            self.a_code_example.errors.append("Cannot submit code if none is selected!")
             return False
         
         # Check if there is a code type and example for answers
-        if not self.answer_code_type and self.answer_code_example:
-            self.answer_code_type.errors.append("Code type is required!")
+        if self.a_figure_type.data == 'code' and not self.a_code_type and not self.a_code_example:
+            self.a_code_type.errors.append("Code type is required!")
+            self.a_code_example.errors.append("Code example is required!")
             return False
-        elif self.answer_code_type and not self.answer_code_example:
-            self.answer_code_example.errors.append("Code example is required!")
+        elif self.a_figure_type.data == 'code' and not self.a_code_type and self.a_code_example:
+            self.a_code_type.errors.append("Code type is required!")
+            return False
+        elif self.a_figure_type.data == 'code' and self.a_code_type and not self.a_code_example:
+            self.a_code_example.errors.append("Code example is required!")
             return False
         
         # Check if there is only one example for answers
-        if self.answer_code_example and self.answer_image_file:
-            self.answer_image_file.errors.append("Can't have image and code examples!")
-            self.answer_code_example.errors.append("Can't have code and image examples!")
+        if self.a_code_example and self.a_image_file:
+            self.a_image_file.errors.append("Can't have image and code examples!")
+            self.a_code_example.errors.append("Can't have code and image examples!")
             return False
         
         return True
