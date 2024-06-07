9fc5c3afbb35795b3c8fa8eae4534676aab43b31
kitze012 <sskitzerow@gmail.com>
2024-06-07 11:36:08 -0500

    Adding initial test files

---
+++ tests/base_test.py
@@ -0,0 +1,28 @@
+from flask_testing import TestCase
+
+from app import init_app
+
+from app.utils import remove_images
+from app.extensions import db
+from app.models.flashcard_model import FlashcardModel, FigureModel
+
+
+class BaseTestCase(TestCase):
+    '''
+    Creates a base test class for test cases
+    '''
+    def create_app(self):
+        # Create and configure the app for testing
+        app = init_app('config.TestingConfig')
+        return app
+
+    def setUp(self):
+        # Set up the database
+        self.client = self.app.test_client()
+        db.create_all()  
+
+    def tearDown(self):
+        # Tear down the database
+        db.session.remove()
+        db.drop_all()
+
