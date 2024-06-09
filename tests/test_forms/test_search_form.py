13a8bca2ad5ee64614cdc72ea26939f58d7b6336
kitze012 <sskitzerow@gmail.com>
2024-06-09 17:13:01 -0500

    Adding search form tests

---
+++ tests/test_forms/test_search_form.py
@@ -0,0 +1,32 @@
+import unittest
+
+from tests.base_test import BaseTestCase
+
+from app.forms.search_form import SearchForm
+
+class Test_Default_Form(BaseTestCase):
+
+    def test_1_search_form(self):
+        '''
+        Tests the search form with no inputs
+        '''
+        form = SearchForm(data={})
+        self.assertFalse(form.validate())
+    #-----------------------------------------------------------------------------------------------------------
+    def test_2_search_form(self):
+        '''
+        Tests the search form with inputs
+        '''
+        form = SearchForm(data={
+            'search': 'Search test'
+        })
+        self.assertTrue(form.validate())
+    #-----------------------------------------------------------------------------------------------------------
+    def test_3_search_form(self):
+        '''
+        Tests the search form with invalid inputs
+        '''
+        form = SearchForm(data={
+            'search': 'Search test' * 10
+        })
+        self.assertFalse(form.validate())
